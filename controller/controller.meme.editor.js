5030c4909ce912d15b00ba0fa04a089de4011bb9
Teperr <omerteperr@gmail.com>
2024-03-16 16:08:35 +0200

    Add initMeme and renderMeme functions

---
+++ controller/controller.meme.editor.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var gElCanvas
+var gCtx
+
+function initMeme() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    console.log('SSDD:')
+    console.log('gElCanvas:', gElCanvas)
+    console.log('gCtx:', gCtx)
+
+    renderMeme()
+
+}
+
+function renderMeme() {
+    const img = new Image()
+    img.src = 'img/meme-imgs (square)/1.jpg'
+
+    img.onload = () => gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+
+}
