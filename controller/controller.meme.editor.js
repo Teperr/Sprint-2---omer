eaca7732ef9154df19e0aed2dbb9c5cbfc92c50e
Teperr <omerteperr@gmail.com>
2024-03-16 17:09:03 +0200

    Add img and text to canvas

--- controller/controller.meme.editor.js
+++ controller/controller.meme.editor.js
@@ -19,6 +19,17 @@ function renderMeme() {
     const img = new Image()
     img.src = 'img/meme-imgs (square)/1.jpg'
 
-    img.onload = () => gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+
+    img.onload = () =>{
+        gCtx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight)
+        
+        const text = 'shira'
+
+        gCtx.fillStyle = 'orange'
+        gCtx.font = '50px Ariel'
+    
+        gCtx.fillText(text, 50, 50)
+        gCtx.storkeText(text, 50, 50)
+    }
 
 }
